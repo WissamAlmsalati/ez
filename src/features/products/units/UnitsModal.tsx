dad8c15160ce26746a6f11d6a68647f36c4308fa
MustZA <mz.aboaisha@gmail.com>
2025-10-24 10:32:08 +0200

    fix: increase items per page in UnitsModal query to improve performance

--- src/features/products/units/UnitsModal.tsx
+++ src/features/products/units/UnitsModal.tsx
@@ -39,7 +39,7 @@ const UnitsModal: React.FC<Props> = ({ open, onClose }) => {
 
   const list = useUnitsQuery({
     page: 1,
-    per_page: 100,
+    per_page: 500,
     search: debounced || undefined,
   });
   const createMut = useCreateUnit();

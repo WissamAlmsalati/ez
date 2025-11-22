9ffdf3f8304a27224f64baa55d80526a2d7cdd8e
MustZA <mz.aboaisha@gmail.com>
2025-11-22 02:43:08 +0200

    feat: enhance layout in AdvertisementDetailPage for better responsiveness

--- src/app/(DashboardLayout)/advertisements/[id]/page.tsx
+++ src/app/(DashboardLayout)/advertisements/[id]/page.tsx
@@ -313,7 +313,7 @@ export default function AdvertisementDetailPage() {
                   </div>
                 </div>
               </div>
-              <div className="flex items-center gap-3 pt-2 justify-end">
+              <div className="flex flex-col md:flex-row md:items-center gap-3 pt-2 md:justify-end">
                 {!deleted && (
                   <Button
                     color="failure"
